dd81e866bb339773080ca7dbced87a8f4cdb7746
Mariano <mariano.padularrosa@gmail.com>
2021-08-13 13:09:59 -0300

    dolar api

--- src/components/CardComponentHooks.js
+++ src/components/CardComponentHooks.js
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from "react";
-import Button from "./button";
+import Button from "./Button";
 
 export default function CardComponentHooks(props) {
   const [count, setCount] = useState(props.initial);
